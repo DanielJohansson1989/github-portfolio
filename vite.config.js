46660113410fea2104486ec061846a21fd0a869a
Daniel Johansson <145423464+DanielJohansson1989@users.noreply.github.com>
2024-03-11 09:55:53 +0100

    Update vite.config.js
    removed github-portfolio/ from path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './github-portfolio/dist',
+  base: './dist',
   plugins: [react()],
 })
